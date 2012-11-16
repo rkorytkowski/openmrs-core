416ca5addf1c50ba65c6982fc55aaceb142d2e44
rkorytkowski <rafal.korytkowski@gmail.com>
2012-11-16 15:04:55 +0100

    JPA Annotations examle for TRUNK-3778

--- api/src/main/java/org/openmrs/GlobalProperty.java
+++ api/src/main/java/org/openmrs/GlobalProperty.java
@@ -13,6 +13,12 @@
  */
 package org.openmrs;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
 import org.openmrs.customdatatype.CustomDatatype;
 import org.openmrs.customdatatype.CustomDatatypeUtil;
 import org.openmrs.customdatatype.CustomValueDescriptor;
@@ -23,25 +29,37 @@ import org.openmrs.customdatatype.SingleCustomValue;
  * Global properties are simple key-value pairs persisted in the database GPs can be thought of as
  * something similar to environment variables used in operating systems.
  */
+@Entity
+@Table(name = "global_property")
 public class GlobalProperty extends BaseOpenmrsObject implements CustomValueDescriptor, SingleCustomValue<GlobalProperty> {
 	
+	@Id
+	@Column(name = "property", length = 255)
 	private String property = "";
 	
+	@Column(name = "property_value", length = 65535, nullable = false)
 	private String propertyValue = "";
 	
+	@Transient
 	private transient Object typedValue;
 	
 	// if true, indicates that setValue has been called, and we need to invoke CustomDatatype's save
+	@Transient
 	private boolean dirty = false;
 	
+	@Column(name = "description", length = 65535)
 	private String description = "";
 	
+	@Column(name = "datatype", length = 255)
 	private String datatypeClassname;
 	
+	@Column(name = "datatype_config", length = 65535)
 	private String datatypeConfig;
 	
+	@Column(name = "preferred_handler", length = 255)
 	private String preferredHandlerClassname;
 	
+	@Column(name = "handler_config", length = 65535)
 	private String handlerConfig;
 	
 	/**
